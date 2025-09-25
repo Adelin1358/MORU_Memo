a412c48c840201e2a5dda1787d1a92a456a058a4
upright_help <pallidamors8250@gmail.com>
2025-09-25 13:23:06 +0900

    [최정우] - 회원가입 입력화면 레이아웃 구성
    #1

---
+++ src/main/java/com/pallidamors/memo/user/UserController.java
@@ -0,0 +1,19 @@
+package com.pallidamors.memo.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+
+
+    @GetMapping("/join")
+    public String joinForm(){
+        return "user/join";
+    }
+
+}
